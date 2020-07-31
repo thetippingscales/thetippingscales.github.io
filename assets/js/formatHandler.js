9fbf08323c9d8f9b0262a641572909cd96038af7
viper12590 <viper1259@gmail.com>
2020-07-31 16:43:31 +0100

    bug fix: if first img has class no imgs get new class

--- assets/js/formatHandler.js
+++ assets/js/formatHandler.js
@@ -1,23 +1,18 @@
-function checkIfClass(items) {
-    for(let i=0; i < items.length; i++) {
-        if(items[i].classList.length > 0) {
-            return false;
-        }
-    }
-    return true;
+function checkIfClass(item) {
+        return item.classList.length <= 0;
 }
 
-function setClassToItems(items, className) {
-    for(let i=0; i < items.length; i++) {
-        items[i].className = className;
-    }
+function setClassToItem(item, className) {
+        item[i].className = className;
 }
 
 document.addEventListener("DOMContentLoaded", function () {
     var imgs = document.getElementsByTagName("img");
-    if(checkIfClass(imgs)) {
-        setClassToItems(imgs, "img-fluid");
-    }
     var blockquotes = document.getElementsByTagName("blockquote");
-    setClassToItems(blockquotes, "font-italic blockquote text-center");
+    for(let i=0; i < imgs.length; i++) {
+        if(checkIfClass(imgs[i])) {
+            setClassToItem(imgs[i], "img-fluid");
+        }
+        setClassToItem(blockquotes[i], "font-italic blockquote text-center");
+    }
 })
