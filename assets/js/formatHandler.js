fc8de489176ac92cc04c388b915921695a2d88d6
viper12590 <viper1259@gmail.com>
2020-07-31 16:32:54 +0100

    set img-fluid only if img has no class

--- assets/js/formatHandler.js
+++ assets/js/formatHandler.js
@@ -1,11 +1,23 @@
-function addClassToTag(tagName, className) {
-    var element = document.getElementsByTagName(tagName);
-    for(let i=0; i < element.length; i++) {
-        element[i].className = className;
+function checkIfClass(items) {
+    for(let i=0; i < items.length; i++) {
+        if(items[i].classList.length > 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function setClassToItems(items, className) {
+    for(let i=0; i < items.length; i++) {
+        items[i].className = className;
     }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    addClassToTag("img","img-fluid");
-    addClassToTag("blockquote", "font-italic blockquote text-center");
+    var imgs = document.getElementsByTagName("img");
+    if(checkIfClass(imgs)) {
+        setClassToItems(imgs, "img-fluid");
+    }
+    var blockquotes = document.getElementsByTagName("blockquote");
+    setClassToItems(blockquotes, "font-italic blockquote text-center");
 })
