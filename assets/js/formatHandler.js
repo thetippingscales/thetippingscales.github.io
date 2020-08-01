30172bbaca067c3c9f2b76920cb4aaddbf4cc61b
viper12590 <viper1259@gmail.com>
2020-08-01 19:46:25 +0100

    responsive embedded video test

--- assets/js/formatHandler.js
+++ assets/js/formatHandler.js
@@ -6,13 +6,25 @@ function setClassToItem(item, className) {
         item.className = className;
 }
 
+function responsiveEmbed(item, className) {
+    var videoWrapper = document.createElement("div");
+    videoWrapper.id = "videoWrapper";
+    videoWrapper.className = className;
+    item.after(videoWrapper);
+    videoWrapper.appendChild(item);
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     var imgs = document.getElementsByTagName("img");
     var blockquotes = document.getElementsByTagName("blockquote");
+    var iframes = document.getElementsByTagName("iframe");
     for(let i=0; i < imgs.length; i++) {
         if(checkIfClass(imgs[i])) {
             setClassToItem(imgs[i], "img-fluid");
         }
+    }
+    for(let i=0; i < iframes.length; i++) {
+        responsiveEmbed(iframes[0],"embed-responsive embed-responsive-16by9");
     }
     for(let i=0; i < blockquotes.length; i++) {
         setClassToItem(blockquotes[i], "font-italic blockquote text-center");
