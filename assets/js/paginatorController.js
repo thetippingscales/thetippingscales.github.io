c14ae28c9ab9c166da766f47d04292e648fcd608
viper12590 <viper1259@gmail.com>
2020-08-09 08:21:42 +0100

    paginator js test 4

--- assets/js/paginatorController.js
+++ assets/js/paginatorController.js
@@ -18,8 +18,16 @@ document.addEventListener("DOMContentLoaded", function () {
         var page_first = pages[1];
         var page_current = document.getElementsByClassName("page-item active")[0];
         var page_last = pages[pages.length - 2];
+        var remove_pages = [];
 
-        pages = pages.filter(page => page === page_first || page === page_current || page === page_last);
+        for(let i=1; i < pages.length - 1; i++) {
+            if(pages[i] !== page_current && pages[i] !== page_first && pages[i] !== page_last) {
+                remove_pages.push(pages[i]);
+            }
+        }
+        for(let i=0; i < remove_pages.length; i++) {
+            pagi_ul.removeChild(remove_pages[i]);
+        }
 
         if(page_current === page_first) {
             pagi_ul.insertBefore(dot3right, page_last);
