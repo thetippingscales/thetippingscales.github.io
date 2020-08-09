fbed16ab6877443dd96fa7763cbdb6826d94e659
viper12590 <viper1259@gmail.com>
2020-08-09 07:24:23 +0100

    pagination js test

---
+++ assets/js/paginatorContoller.js
@@ -0,0 +1,40 @@
+function createDot3() {
+    let dot3 = document.createElement("li");
+    dot3.className = "page-item";
+    let dot3Child = document.createElement("a");
+    dot3Child.className = "page-link";
+    dot3Child.text = "...";
+    dot3.appendChild(dot3Child);
+    return dot3;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    var pagi_nav = document.getElementById("pagination_nav");
+    var pagi_ul = pagi_nav.firstElementChild;
+    var pages = pagi_ul.children;
+    if(pages.length > 4) {
+        var dot3left = createDot3();
+        var dot3right = createDot3();
+        var page_first = pages[1];
+        var page_current = document.getElementsByClassName("page-item active")[0];
+        var page_last = pages[pages.length - 2];
+
+        for(let i=1; i < pages.length - 1; i++) {
+            if(pages[i] !== page_current || pages[i] !== page_first || pages[i] !== page_last) {
+                pagi_ul.removeChild(pages[i]);
+            }
+        }
+        if(page_current === page_first) {
+            pagi_ul.insertBefore(dot3right, page_last);
+        }
+        else if(page_current === page_last) {
+            pagi_ul.insertBefore(dot3left, page_current);
+        }
+        else {
+            pagi_ul.insertBefore(dot3left, page_current);
+            pagi_ul.insertBefore(dot3right, page_last);
+        }
+    }
+
+
+})
