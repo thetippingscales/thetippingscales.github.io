9d12afd3bbf24a05e8ede6b7a9952c48f706c6e4
viper12590 <viper1259@gmail.com>
2020-08-09 10:04:29 +0100

    limiting js from liquid

--- assets/js/paginatorController.js
+++ assets/js/paginatorController.js
@@ -12,48 +12,45 @@ document.addEventListener("DOMContentLoaded", function () {
     var pagi_ul = document.getElementById("pagination_nav").firstElementChild;
     var pages = pagi_ul.children;
 
-    if(pages.length - 2 > 7) {
-        var ellipsisLeft = createEllipsis();
-        var ellipsisRight = createEllipsis();
+    var ellipsisLeft = createEllipsis();
+    var ellipsisRight = createEllipsis();
 
-        var last_num = pages.length - 2;
-        var page_current = document.getElementsByClassName("page-item active")[0];
-        var page_last = pages[last_num];
-        var remove_pages = [];
-        var current_index = 0;
+    var last_num = pages.length - 2;
+    var page_current = document.getElementsByClassName("page-item active")[0];
+    var page_last = pages[last_num];
+    var remove_pages = [];
+    var current_index = 0;
 
-        for(let i=1; i < pages.length - 1; i++) {
-            if(i !== 1 && i !== last_num && pages[i] !== page_current && pages[i - 1] !== page_current && pages[i + 1] !== page_current) {
-                remove_pages.push(pages[i]);
-            }
-
-        }
-        for(let i=0; i < remove_pages.length; i++) {
-            pagi_ul.removeChild(remove_pages[i]);
-        }
-        for(let i=0; i < pages.length; i++) {
-            if(pages[i] === page_current) {
-                current_index = i;
-            }
+    for(let i=1; i < pages.length - 1; i++) {
+        if(i !== 1 && i !== last_num && pages[i] !== page_current && pages[i - 1] !== page_current && pages[i + 1] !== page_current) {
+            remove_pages.push(pages[i]);
         }
 
-        switch (page_current.firstElementChild.textContent) {
-            case "1":
-            case "2":
-            case "3":
-                pagi_ul.insertBefore(ellipsisRight, page_last);
-                break;
-            case (last_num - 2).toString():
-            case (last_num - 1).toString():
-            case (last_num).toString():
-                pagi_ul.insertBefore(ellipsisLeft, pages[current_index - 1]);
-                break;
-            default:
-                pagi_ul.insertBefore(ellipsisLeft, pages[current_index - 1]);
-                pagi_ul.insertBefore(ellipsisRight, page_last);
-                break;
+    }
+    for(let i=0; i < remove_pages.length; i++) {
+        pagi_ul.removeChild(remove_pages[i]);
+    }
+    for(let i=0; i < pages.length; i++) {
+        if(pages[i] === page_current) {
+            current_index = i;
         }
     }
 
+    switch (page_current.firstElementChild.textContent) {
+        case "1":
+        case "2":
+        case "3":
+            pagi_ul.insertBefore(ellipsisRight, page_last);
+            break;
+        case (last_num - 2).toString():
+        case (last_num - 1).toString():
+        case (last_num).toString():
+            pagi_ul.insertBefore(ellipsisLeft, pages[current_index - 1]);
+            break;
+        default:
+            pagi_ul.insertBefore(ellipsisLeft, pages[current_index - 1]);
+            pagi_ul.insertBefore(ellipsisRight, page_last);
+            break;
+    }
 
 })
